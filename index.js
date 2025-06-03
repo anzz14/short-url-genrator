51ee8cf4ff6e04ae106bec6d2dd4b5aadac8743b
anzz14 <anasnajam14@gmail.com>
2025-06-03 23:06:20 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express= require('express');
+const urlRoute= require('./routes/url');
+const {connectToMongoDB}= require('./connect');
+const app= express();
+const URL=require('./models/url');
+const PORT= 8001;
+
+connectToMongoDB('mongodb://localhost:27017/short-url')
+.then(()=>console.log("mongoDB connected"));
+
+app.use(express.json()); 
+
+app.use("/url", urlRoute);
+
+app.get('/:shortId', async (req, res)=>{
+    const shortId = req.params.shortId;
+    const entry= await URL.findOneAndUpdate({
+        shortId
+    }, {$push: {
+        visitHistory: {timestamp: Date.now()},
+    },
+}
+);
+res.redirect(entry.redirectURL);
+})
+
+app.listen(PORT, ()=>console.log(`server has started at PORT: ${PORT}`));
